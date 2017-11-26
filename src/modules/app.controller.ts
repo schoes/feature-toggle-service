9533ad296e8603207db8ba9e2c5164f557f4ab34
Sven Schoeni <sven.schoeni@gmail.com>
2017-11-26 14:51:54 +0100

    feat(service): created interface

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,9 +1,27 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Put, Delete, Controller, Req, Body } from '@nestjs/common';
+import { FeatureToggle } from './interfaces';
 
 @Controller()
 export class AppController {
-	@Get()
-	root(): string {
-    return 'Hello World!';
+  @Get()
+  root(): Array<FeatureToggle> {
+    return this.getFeatureToggles();
   }
+  @Get('/feature-toggles')
+  getFeatureToggles(): Array<FeatureToggle> {
+    return [];
+  }
+  @Get('/feature-toggles/:toggleID')
+  getFeatureToggleById( @Req() request) {
+
+  }
+  @Delete('/feature-toggles/:toggleID')
+  removeFeatureToggleById( @Req() request) {
+
+  }
+  @Put('/feature-toggles')
+  createFeatureToggle( @Body() featureToggle: FeatureToggle) {
+
+  }
+
 }
