58f6a9c6ba6c5cfeb0034f927466455dea8c9081
Sven Schoeni <sven.schoeni@gmail.com>
2017-11-26 14:53:05 +0100

    feat(interface): add interface

---
+++ src/modules/interfaces.ts
@@ -0,0 +1,10 @@
+export class FeatureToggle{
+    constructor(){
+        this.isActivated = false;
+    }
+    activationDate:string;
+    deactivationDate:string;
+    toggleId:string;
+    isActivated:boolean;
+    uuid:string;
+}
