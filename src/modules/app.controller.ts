b2c25432394f2d78ab0dce611f735baa4cd38666
Sven Schoeni <sven.schoeni@gmail.com>
2017-11-27 08:59:26 +0100

    refactor(controller): add mongodb

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,27 +1,65 @@
-import { Get, Put, Delete, Controller, Req, Body } from '@nestjs/common';
-import { FeatureToggle } from './interfaces';
+import {Body, Controller, Delete, Get, Put, Req} from '@nestjs/common';
+import * as assert from 'assert';
+import * as Mongo from 'mongodb';
+import {FeatureToggle} from './interfaces';
+
+const MongoClient = Mongo.MongoCLient;
+const url = 'ds121716.mlab.com:21716/featuretoggle';
 
 @Controller()
 export class AppController {
-  @Get()
-  root(): Array<FeatureToggle> {
-    return this.getFeatureToggles();
-  }
-  @Get('/feature-toggles')
-  getFeatureToggles(): Array<FeatureToggle> {
-    return [];
-  }
-  @Get('/feature-toggles/:toggleID')
-  getFeatureToggleById( @Req() request) {
 
-  }
-  @Delete('/feature-toggles/:toggleID')
-  removeFeatureToggleById( @Req() request) {
+    constructor() {
+        MongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            console.log('Connected successfully to server');
 
-  }
-  @Put('/feature-toggles')
-  createFeatureToggle( @Body() featureToggle: FeatureToggle) {
+            db.close();
+        });
+    }
 
-  }
+    @Get()
+    public root(): Array<FeatureToggle> {
+        return this.getFeatureToggles();
+    }
+
+    @Get('/feature-toggles')
+    public getFeatureToggles(): Array<FeatureToggle> {
+        let toggles: Array<FeatureToggle> = [];
+        MongoClient.connect()
+            .then((db) => {
+                let collection = db.collection('featuretoggles');
+                collection.fin({}).toArray()
+                    .then((result) => {
+                        toggles = result;
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        return [];
+                    });
+            })
+            .catch((error) => {
+                console.error(error);
+                return [];
+            });
+        // TODO remove when promise is ready
+        return [];
+    }
+
+    @Get('/feature-toggles/:toggleID')
+    public getFeatureToggleById(@Req() request) {
+
+    }
+
+    @Delete('/feature-toggles/:toggleID')
+    public removeFeatureToggleById(@Req() request) {
+
+    }
+
+    @Put('/feature-toggles')
+    public createFeatureToggle(@Body() featureToggle: FeatureToggle) {
+
+    }
+
 
 }
